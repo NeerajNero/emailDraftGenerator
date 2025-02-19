3c469874ce1e7e6ca2002cfbe618f448b4acf4e2
NeerajNero <neerajnero9066@gmail.com>
2025-02-19 20:21:08 +0530

    updated code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className='bg-light'>
     <div className='container py-3'> 
-        <h3>Email Draft Generator</h3>
+        <h3>Email Draft Generator.</h3>
     </div>
     </div>
   )
