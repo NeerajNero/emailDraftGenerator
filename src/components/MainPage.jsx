3ed1bf93cc7250177bdd00ccd578524bf95e50e0
NeerajNero <neerajnero9066@gmail.com>
2025-04-27 11:14:17 +0530

    updated UI

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -74,9 +74,9 @@ const MainPage = () => {
       });
   };
   return (
-    <main className='container mt-3'>
+    <main className='container mt-2'>
       <div className='d-flex filterStyle'>
-      <div className='my-3 me-3'>
+      <div className='my-2 me-3'>
         <select onChange={handleDisplaySubSection} className='form-select'>
           <option value="" autoFocus>Select Issue</option>
           <option value="Disbursement issues">Disbursement issues</option>
@@ -91,20 +91,20 @@ const MainPage = () => {
           <option value="Harassment /fraud">Harassment /fraud</option>
         </select>
         </div>
-        {subSection.length > 0 && <div className='mx-3 my-3'>
+        {subSection.length > 0 && <div className='mx-3 mt-2'>
           <select onChange={(e) => setSubSelectedSection(e.target.value)} defaultValue={subSection[0]} className='form-select'>
             {subSection.map((sub,i) => <option key={i} value={sub}>{sub}</option>)}
           </select>
           </div>}
           </div>
-          <div className='d-flex gap-3 mt-3 filterStyle'>
+          <div className='d-flex gap-3 mt-1 filterStyle'>
               <span><input className='form-control' required type='text' placeholder='Customer Name' value={customerName} onChange={handleChange}/></span>
               <span><input className='form-control' required type='text' placeholder='App Name' value={appName} onChange={(e) => setAppName(e.target.value)}/></span>
               <button className='btn btn-primary' onClick={handleGenerateEmail}>{emailDraftDataStatus === "loading" ? <div className="spinner-border text-secondary" role="status" style={{height: "5px"}}>
   <span className="visually-hidden">Loading....</span>
 </div> : "Generate Email"}</button>
           </div>
-          <div className='my-3'>
+          <div className='my-2'>
             {}
             {emailDraftDataStatus === "loading" ? <Loader /> : emailDraftData && <div className='list-group col-md-10'><div className='list-group-item'><div className='d-flex gap-3 align-items-center'><h4 className='mt-2'>Generated Email </h4><button onClick={handleCopyEmail} className='btn btn-secondary'>Copy</button> {copiedEmail && "Copied!"}</div><hr/><p dangerouslySetInnerHTML={{ __html: emailDraftData.emailBody}}></p></div><div className='list-group'><div className='list-group-item'><div className='d-flex gap-3 align-items-center'><h4 className='mt-2'>Suggested Note</h4><button onClick={() => handleCopySuggestedNotes(emailDraftData?.suggestedNotes)} className='btn btn-secondary'>Copy</button> {copied && "Copied!"}</div><hr/><p>{emailDraftData.suggestedNotes}</p></div></div></div>}
           </div>
